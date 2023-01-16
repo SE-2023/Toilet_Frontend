7e25b04ec96d134b1192e0ada7c44ab1a2cd01d2
Poramat Wongphueng <98012298+PoramatWong@users.noreply.github.com>
2023-01-16 13:41:19 +0700

    add

--- App.tsx
+++ App.tsx
@@ -16,7 +16,7 @@
 //     ...StyleSheet.absoluteFillObject,
 //   },
 // });
-//aom66666666666666666
+//aom68888
 // const styles = StyleSheet.create({});
 import {StyleSheet, Text, View, Button, Alert} from 'react-native';
 import {ActivityIndicator} from 'react-native';
