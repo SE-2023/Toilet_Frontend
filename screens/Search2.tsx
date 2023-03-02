45ccca25c6002ce07837d04e0bbf333a58434c79
Thapanee88 <76778207+Thapanee88@users.noreply.github.com>
2023-03-02 17:33:13 +0700

    search

--- screens/Search2.tsx
+++ screens/Search2.tsx
@@ -18,6 +18,8 @@ import {
   Tote,
   GasPump,
   House,
+  Star,
+  Clock,
 } from 'phosphor-react-native';
 import wc from '../assets/wc.png';
 import { searchToilet } from '../services/search'
@@ -31,7 +33,6 @@ const Search2 = () => {
 
   const hardleSearch = async () => {
     setIsloading(true);
-    
     try {
       setResultPublic([]);
       setResultPrivate([]);
@@ -40,7 +41,7 @@ const Search2 = () => {
       // console.log(toilets.privateToilet);
       setResultPublic(toilets.publicToilet);
       setResultPrivate(toilets.privateToilet);
-    } catch (err:any) {
+    } catch (err: any) {
       setErrorResult(err.msg);
     }
     setIsloading(false);
@@ -62,21 +63,70 @@ const Search2 = () => {
             <XCircle size={22} weight="fill" color="#BABCCA" />
           </TouchableOpacity>
         </View>
-        { isLoading ? (
-        <ActivityIndicator />
-        ) : ResultPublic?.length > 0 ? (
-          <FlatList
-            data={ResultPublic}
-            keyExtractor={(item:any) => String(item._id)}
-            renderItem={({item}) => 
-              { return <Text> {item.title} </Text>}
-            }
-          />
-        ) : (
-          <Text>{errorResult}</Text>
-        )
-      }
       </View>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : ResultPublic && ResultPublic.length > 0 ? (
+        <View style={styles.contentContainer}>
+          {ResultPublic.map((item) => (
+            <View key={item._id} style={styles.content}>
+              <View style={styles.itemLeftTop}>
+                <View style={styles.tagFree}>
+                  <Text style={styles.textFree}>฿ Free</Text>
+                </View>
+                <View style={styles.tagHandicap}>
+                  <Wheelchair
+                    size={10}
+                    weight="fill"
+                    color="#00845A"
+                    style={{
+                      marginRight: 2,
+                      marginLeft: 6,
+                    }}
+                  />
+                  <Text style={styles.textHandicap}>Handicap access</Text>
+                </View>
+
+                <View style={styles.tagType}>
+                  <Text style={styles.textType}>Public</Text>
+                </View>
+              </View>
+
+              <Text style={styles.placeName}>Place Name</Text>
+
+              <View style={styles.itemBottom}>
+                <View style={styles.itemLeftBottom}>
+                  <Clock
+                    size={14}
+                    weight="fill"
+                    color="#31C596"
+                    style={{
+                      marginRight: 5,
+                    }}
+                  />
+                  <Text style={styles.time}>
+                    00:00 - 00:00
+                  </Text>
+                </View>
+                <View style={styles.itemRightBottom}>
+                  <Star
+                    size={14}
+                    weight="fill"
+                    color="#FBD17B"
+                    style={{
+                      marginRight: 2,
+                    }}
+                  />
+                  <Text style={styles.rate}>5.0</Text>
+                </View>
+              </View>
+            </View>
+            </View>
+      )}
+      ) : (
+      <Text>{errorResult}</Text>
+      )
+      }
     </View>
   )
 }
@@ -116,5 +166,111 @@ const styles = StyleSheet.create({
     top: 8.5,
     right: 14,
     zIndex: 1,
+  },
+
+
+  contentContainer: {
+    marginVertical: 20,
+    backgroundColor: '#2C2F4A',
+    borderRadius: 8,
+    borderColor: '#ccc',
+    paddingHorizontal: 14,
+    paddingBottom: 14,
+    paddingTop: 10
+  },
+  content: {
+    width: '100%',
+  },
+  itemLeftTop: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+  },
+
+  // Tag Free
+  tagFree: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+    marginBottom: 15,
+    marginRight: 12,
+    backgroundColor: '#0BF8AD',
+    borderRadius: 20,
+  },
+  textFree: {
+    fontFamily: 'Fredoka-Regular',
+    fontSize: 12,
+    color: '#00845A',
+    paddingLeft: 6,
+    paddingRight: 6,
+    paddingVertical: 2,
+  },
+
+  // Tag Handicap
+  tagHandicap: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+    marginBottom: 15,
+    marginRight: 12,
+    backgroundColor: '#0BF8AD',
+    borderRadius: 20,
+  },
+  textHandicap: {
+    fontFamily: 'Fredoka-Regular',
+    fontSize: 12,
+    color: '#00845A',
+    paddingRight: 6,
+    paddingVertical: 2,
+  },
+
+  // Tag Type
+  tagType: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+    backgroundColor: '#CACCDA',
+    borderRadius: 20,
+  },
+  textType: {
+    fontFamily: 'Fredoka-Regular',
+    fontSize: 12,
+    color: '#555568',
+    marginLeft: 6,
+    paddingRight: 6,
+    paddingVertical: 2,
+  },
+
+  placeName: {
+    fontFamily: 'Fredoka-Medium',
+    fontSize: 16,
+    color: '#fff',
+    marginBottom: 8,
+  },
+
+  itemBottom: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  itemLeftBottom: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+  },
+  time: {
+    fontFamily: 'Fredoka-Regular',
+    fontSize: 14,
+    color: '#ABADBB',
+  },
+  itemRightBottom: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+  },
+  rate: {
+    fontFamily: 'Fredoka-Regular',
+    fontSize: 14,
+    color: '#fff',
   },
 })
