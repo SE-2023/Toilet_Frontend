8de5e6e38d3782d5b8fcce81bd8530df6fb60883
Thanakrit sangsuwan <191144zs456zs@gmail.com>
2023-03-31 12:56:13 +0700

    Revert "Update"
    This reverts commit f9b9536460c622baf5531acb5a6f1ac78a910e58.

--- screens/AddList.tsx
+++ screens/AddList.tsx
@@ -13,16 +13,15 @@ import React, {useEffect, useState} from 'react';
 import bgSUKA from '../assets/bgSUKA_4.png';
 import ContentMyList from '../components/ContentMyList';
 import {getProfile} from '../services/auth';
-import {getMyList, deleteMyList} from '../services/myList';
+import {getMyList,deleteMyList} from '../services/myList';
 import {X} from 'phosphor-react-native';
 import Modal from 'react-native-modal';
 import BrokenHeart from '../assets/BrokenHeart.png';
 import ButtonHeart from '../components/ButtonHeart';
-import {useNavigation} from '@react-navigation/native';
-import {NativeStackNavigationProp} from '@react-navigation/native-stack';
-import {BottomTabParamList} from '../stacks/BottomTabStack';
-import {HomeParamList} from '../stacks/HomeStack';
-import NotList from '../components/NotList';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { BottomTabParamList } from '../stacks/BottomTabStack';
+import { HomeParamList } from '../stacks/HomeStack';
 
 export interface IProfile {
   _id: string;
@@ -43,10 +42,9 @@ const AddList = () => {
   const [myList, setMyList] = useState<myList[]>([]);
   const [checkData, setCheckData] = useState('');
   const [modal, setModal] = useState(false);
-  const [myListID, setMyListID] = useState('');
-  const [first, setfirst] = useState(Boolean);
-  const navigation =
-    useNavigation<NativeStackNavigationProp<BottomTabParamList>>();
+  const[myListID, setMyListID] = useState('');
+  const [first, setfirst] = useState(Boolean)
+  const navigation = useNavigation<NativeStackNavigationProp<BottomTabParamList>>();
   const getUserProfile = async () => {
     const {data} = await getProfile();
 
@@ -56,14 +54,15 @@ const AddList = () => {
   };
   useEffect(() => {
     getUserProfile();
-  }, [modal]);
+  },[modal]);
 
   useEffect(() => {
     const unsubscribe = navigation.addListener('focus', () => {
       getUserProfile();
     });
     return unsubscribe;
-  }, [navigation]);
+    
+  }, [navigation]);   
   const RenderMyList = (): JSX.Element | null => {
     const navigation =
       useNavigation<NativeStackNavigationProp<HomeParamList>>();
@@ -93,27 +92,27 @@ const AddList = () => {
               };
               return (
                 <TouchableOpacity key={index} onPress={onClick}>
-                  <ContentMyList
-                    myListId={item._id}
-                    _id={item.myListPrivate[0]._id}
-                    latitude={item.myListPrivate[0].latitude}
-                    longitude={item.myListPrivate[0].longitude}
-                    title={item.myListPrivate[0].title}
-                    contact={item.myListPrivate[0].contact}
-                    cost={item.myListPrivate[0].cost}
-                    handicap={item.myListPrivate[0].handicap}
-                    free={item.myListPrivate[0].free}
-                    type={item.myListPrivate[0].type}
-                    timeOpen={item.myListPrivate[0].timeOpen}
-                    timeClose={item.myListPrivate[0].timeClose}
-                    toiletpicture={item.myListPrivate[0].toiletpicture}
-                    onSelected={value => {
-                      setModal(value);
-                    }}
-                    onClick={value => {
-                      setMyListID(value);
-                    }}
-                  />
+                <ContentMyList
+                  myListId={item._id}
+                  _id={item.myListPrivate[0]._id}
+                  latitude={item.myListPrivate[0].latitude}
+                  longitude={item.myListPrivate[0].longitude}
+                  title={item.myListPrivate[0].title}
+                  contact={item.myListPrivate[0].contact}
+                  cost={item.myListPrivate[0].cost}
+                  handicap={item.myListPrivate[0].handicap}
+                  free={item.myListPrivate[0].free}
+                  type={item.myListPrivate[0].type}
+                  timeOpen={item.myListPrivate[0].timeOpen}
+                  timeClose={item.myListPrivate[0].timeClose}
+                  toiletpicture={item.myListPrivate[0].toiletpicture}
+                  onSelected={value => {
+                    setModal(value);
+                  }}
+                  onClick = {value =>{
+                    setMyListID(value);
+                  }}
+                />
                 </TouchableOpacity>
               );
             }
@@ -139,27 +138,27 @@ const AddList = () => {
               };
               return (
                 <TouchableOpacity key={index} onPress={onClick}>
-                  <ContentMyList
-                    myListId={item._id}
-                    _id={item.myListPublic[0]._id}
-                    latitude={item.myListPublic[0].latitude}
-                    longitude={item.myListPublic[0].longitude}
-                    title={item.myListPublic[0].title}
-                    contact={item.myListPublic[0].contact}
-                    cost={item.myListPublic[0].cost}
-                    handicap={item.myListPublic[0].handicap}
-                    free={item.myListPublic[0].free}
-                    type={item.myListPublic[0].type}
-                    timeOpen={item.myListPublic[0].timeOpen}
-                    timeClose={item.myListPublic[0].timeClose}
-                    toiletpicture={item.myListPublic[0].toiletpicture}
-                    onSelected={value => {
-                      setModal(value);
-                    }}
-                    onClick={value => {
-                      setMyListID(value);
-                    }}
-                  />
+                <ContentMyList
+                  myListId={item._id}
+                  _id={item.myListPublic[0]._id}
+                  latitude={item.myListPublic[0].latitude}
+                  longitude={item.myListPublic[0].longitude}
+                  title={item.myListPublic[0].title}
+                  contact={item.myListPublic[0].contact}
+                  cost={item.myListPublic[0].cost}
+                  handicap={item.myListPublic[0].handicap}
+                  free={item.myListPublic[0].free}
+                  type={item.myListPublic[0].type}
+                  timeOpen={item.myListPublic[0].timeOpen}
+                  timeClose={item.myListPublic[0].timeClose}
+                  toiletpicture={item.myListPublic[0].toiletpicture}
+                  onSelected={value => {
+                    setModal(value);
+                  }}
+                  onClick = {value =>{
+                    setMyListID(value);
+                  }}
+                />
                 </TouchableOpacity>
               );
             }
@@ -167,11 +166,7 @@ const AddList = () => {
         </>
       );
     } else {
-      return (
-        <View style={styles.notList}>
-          <NotList />
-        </View>
-      );
+      return null;
     }
   };
 
@@ -199,11 +194,7 @@ const AddList = () => {
             <Text style={styles.titlePopup}>
               Do you want to delete this toilet ?
             </Text>
-            <TouchableOpacity
-              onPress={() => {
-                deleteMyList(myListID), setModal(false);
-              }}
-              style={styles.btnYes}>
+            <TouchableOpacity onPress={() => {deleteMyList(myListID),setModal(false)}} style={styles.btnYes}>
               <Text style={styles.textYes}>YES, DELETE</Text>
             </TouchableOpacity>
             <TouchableOpacity onPress={() => setModal(false)}>
@@ -219,9 +210,6 @@ const AddList = () => {
 export default AddList;
 
 const styles = StyleSheet.create({
-  notList: {
-    marginTop: '55%',
-  },
   container: {
     flex: 1,
     backgroundColor: '#E5EAFA',
